c051feb5bfe5385d9109870a8d8a88df0aca2f85
space-9097 <88567941+space-9097@users.noreply.github.com>
2021-08-10 14:08:31 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  for (let i = 0; i <marks.length; i++) {
+    if (marks[i] >= 45) {
+      console.log(marks[i])
+    }
+    // const element = array[i];
+    
+  }
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
